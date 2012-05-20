85d525fc0f9517ee7bc854bd620b3501a7d2d8ce
Jason Adam Young <jasonadamyoung@gmail.com>
2012-05-20 10:31:02 -0400

    code epoch dates

---
+++ app/models/epoch_date.rb
@@ -0,0 +1,19 @@
+# === COPYRIGHT:
+#  Copyright (c) North Carolina State University
+#  Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+#  BSD(-compatible)
+#  see LICENSE file
+
+class EpochDate
+  GA_START                      = Date.parse('2007-02-23')
+  WWW_LAUNCH                    = Date.parse('2008-02-21')
+  POST_PANDA                    = Date.parse('2011-02-28')
+  WWW_URL_REVAMP                = Date.parse('2011-03-19')
+  
+  # actual date is one day prior
+  CREATE_PILOT_WIKI_MIGRATION   = Date.parse('2011-05-06')
+  CREATE_FIRST_WIKI_MIGRATION   = Date.parse('2011-06-18')
+  CREATE_FAQ_MIGRATION          = Date.parse('2011-06-23')
+  CREATE_FINAL_WIKI_MIGRATION   = Date.parse('2011-07-09')
+end
