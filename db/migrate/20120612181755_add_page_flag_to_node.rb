e05dd51a14be897d277074df8aac00c3211b9b63
Jason Adam Young <jasonadamyoung@gmail.com>
2012-06-17 22:28:16 -0400

    migration re-work

--- db/migrate/20120612181755_add_page_flag_to_node.rb
+++
@@ -1,6 +0,0 @@
-class AddPageFlagToNode < ActiveRecord::Migration
-  def change
-    add_column "nodes", "has_page", :boolean, :default => 0
-    add_index "nodes", ["has_page"], :name => "page_flag_ndx"
-  end
-end
--- db/migrate/20120615171220_dump_resource_tags.rb
+++
@@ -1,9 +0,0 @@
-class DumpResourceTags < ActiveRecord::Migration
-  def up
-    remove_column('pages','resource_tag_names')
-    rename_column('page_taggings','resource_tag_id','tag_id')
-    rename_column('week_totals','resource_tag_id','tag_id')
-    rename_column('total_diffs','resource_tag_id','tag_id')
-    drop_table('resource_tags')
-  end
-end
