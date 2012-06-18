2383b1c5880b1d2f8e093ab15d8465f6c321c29f
Jason Adam Young <jasonadamyoung@gmail.com>
2012-06-18 17:35:39 -0400

    fix accessible attributes

--- app/models/update_time.rb
+++ app/models/update_time.rb
@@ -7,7 +7,7 @@
 
 class UpdateTime < ActiveRecord::Base
   serialize :additionaldata
-  attr_accessible :item,:run_time,:additionaldata
+  attr_accessible :item,:run_time,:additionaldata,:operation
     
   def self.log(item,operation,run_time,additionaldata=nil)
     self.create(:item => item, :operation => operation, :run_time => run_time, :additionaldata => additionaldata)
