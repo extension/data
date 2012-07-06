d4e57f2ba160f0495bdf828106937b3d001a21f1
Jason Adam Young <jasonadamyoung@gmail.com>
2012-07-05 20:26:35 -0400

    remove google viz chart code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -59,34 +59,6 @@ class PagesController < ApplicationController
     end
   end
 
-  def traffic_chart
-    @page = Page.find_by_id(params[:id])
-    
-    
-    data_table = GoogleVisualr::DataTable.new    
-    data_table.new_column('date', 'Date')
-    data_table.new_column('number', 'Views')
-    
-    week_stats = {}
-    @page.week_stats.order('yearweek').map do |ws|
-      yearweek_string = "#{ws.year}-" + "%02d" % ws.week 
-      week_stats[yearweek_string] = ws.unique_pageviews
-    end
-    
-    week_stats_data = @page.traffic_stats_data
-    data_table.add_rows(week_stats_data.size)
-    row_count = 0
-    week_stats_data.each do |date,value|
-      data_table.set_cell(row_count,0,date)
-      data_table.set_cell(row_count,1,value)
-      row_count += 1
-    end
-  
-    options = { :width => 800, :height => 180,  :legend => 'bottom', :pointSize => 0 }
-    @chart = GoogleVisualr::Interactive::LineChart.new(data_table, options)
-    return render(:layout => false)
-  end
-  
   def panda_impact_summary
     if(!params[:weeks].nil?)
       @panda_comparison_weeks =  params[:weeks].to_i
