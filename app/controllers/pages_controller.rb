f5502575de0b863974c1033673c1b38fd0e59dc2
Jason Adam Young <jasonadamyoung@gmail.com>
2012-08-05 22:21:48 -0400

    finish of index restructuring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -59,15 +59,15 @@ class PagesController < ApplicationController
     when 'viewed'
       @page_title = "Viewed #{list_type}"
       @page_title_display = "Viewed #{list_type}"
-      @endpoint = 'viewed'
+      @endpoint = "Viewed #{@datatype.pluralize}"
     when 'unviewed'
       @page_title = "Unviewed #{list_type}"
       @page_title_display = "Unviewed #{list_type}"
-      @endpoint = 'unviewed'
+      @endpoint = "Unviewed #{@datatype.pluralize}"
     else
       @page_title = "All #{list_type}"
       @page_title_display = "All #{list_type}"
-      @endpoint = 'all'
+      @endpoint = "All #{@datatype.pluralize}"
     end      
 
   end
@@ -90,15 +90,15 @@ class PagesController < ApplicationController
     when 'viewed'
       @page_title = "Viewed #{list_type} for Group ##{@group.id}"
       @page_title_display = "Viewed #{list_type} for #{@group.name}"
-      @endpoint = 'viewed'
+      @endpoint = "Viewed #{@datatype.pluralize}"
     when 'unviewed'
       @page_title = "Unviewed #{list_type} for Group ##{@group.id}"
       @page_title_display = "Unviewed #{list_type} for #{@group.name}"
-      @endpoint = 'unviewed'
+      @endpoint = "Unviewed #{@datatype.pluralize}"
     else
       @page_title = "All #{list_type}"
       @page_title_display = "All #{list_type}"
-      @endpoint = 'all'
+      @endpoint = "All #{@datatype.pluralize}"
     end    
       
   end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -5,8 +5,5 @@
 # see LICENSE file
 module GroupsHelper
 
-  def jqplot_group_traffic_data_by_datatype(group,datatype) 
-    group.traffic_stats_data_by_datatype(datatype).to_json.html_safe
-  end
 end
   
