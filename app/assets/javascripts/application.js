c275467c70c8767fc0b22c02ea5ec5300c22727d
Jason Adam Young <jayoung@extension.org>
2012-08-23 09:53:11 -0400

    bundle update - includes bootstrap update to bootstrap 2.1
    http://blog.getbootstrap.com/2012/08/20/bootstrap-2-1-0-released/
    
    I like the white navbar.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require twitter/bootstrap
 //= require jqplot
 //= require jqplot/plugins/dateAxisRenderer
 //= require jqplot/plugins/highlighter
