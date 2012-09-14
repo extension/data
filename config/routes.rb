04d7d292e7a06fe7e67e27e396c7e1fad01b8aaf
Jason Adam Young <jayoung@extension.org>
2012-09-14 13:53:35 -0400

    use the to: syntax, so much nicer

--- config/routes.rb
+++ config/routes.rb
@@ -14,16 +14,16 @@ Positronic::Application.routes.draw do
   end
 
   # pretty url matchers
-  match '/pages/graphs/:datatype/:group' => 'pages#graphs', :as => 'graphs_pages', :via => :get
-  match '/pages/graphs/:datatype' => 'pages#graphs', :as => 'graphs_pages', :via => :get
-  match '/pages/datatype/:datatype' => 'pages#datatype', :as => 'datatype_pages', :via => :get
-  match '/pages/group/:id/:datatype' => 'pages#groupdatatype', :as => 'datatype_group', :via => :get
+  match '/pages/graphs/:datatype/:group', to: 'pages#graphs', :as => 'graphs_pages', :via => :get
+  match '/pages/graphs/:datatype', to: 'pages#graphs', :as => 'graphs_pages', :via => :get
+  match '/pages/datatype/:datatype', to:'pages#datatype', :as => 'datatype_pages', :via => :get
+  match '/pages/group/:id/:datatype', to:'pages#groupdatatype', :as => 'datatype_group', :via => :get
   
   resources :groups, :only => [:index, :show] 
   
   # data routes
   scope "data" do
-    match "/groups" => "data#groups", :as => 'data_groups'
+    match "/groups", to: "data#groups", :as => 'data_groups'
   end
   
 
