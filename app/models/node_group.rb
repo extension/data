cbfdd5b09cc93eb6008c83ac5b0d82864c8402c4
Jason Adam Young <jayoung@extension.org>
2012-12-11 14:27:45 -0500

    don't fail on missing groups, fixes #1

--- app/models/node_group.rb
+++ app/models/node_group.rb
@@ -16,11 +16,13 @@ class NodeGroup < ActiveRecord::Base
     group_mappings = {}
     Group.all.map{|g| group_mappings[g.create_gid] = g.id}
     CreateGroupNode.where(:entity_type => 'node').all.each do |groupnode|
-      insert_list = []
-      insert_list << groupnode.entity_id
-      insert_list << group_mappings[groupnode.group_audience_gid.to_i]
-      insert_list << ActiveRecord::Base.quote_value(groupnode.created_at.to_s(:db))
-      insert_values << "(#{insert_list.join(',')})"
+      if(group_mappings[groupnode.group_audience_gid.to_i])
+        insert_list = []
+        insert_list << groupnode.entity_id
+        insert_list << group_mappings[groupnode.group_audience_gid.to_i]
+        insert_list << ActiveRecord::Base.quote_value(groupnode.created_at.to_s(:db))
+        insert_values << "(#{insert_list.join(',')})"
+      end
     end
     insert_sql = "INSERT INTO #{self.table_name} (node_id,group_id,created_at) VALUES #{insert_values.join(',')};"
     self.connection.execute(insert_sql)
