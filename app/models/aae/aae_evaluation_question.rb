d649312841d37e4ace65b049a54f9f79b61302ca
Jason Adam Young <jayoung@extension.org>
2013-06-05 15:46:07 -0400

    use the mean of all the open dollar numbers we find

--- app/models/aae/aae_evaluation_question.rb
+++ app/models/aae/aae_evaluation_question.rb
@@ -41,7 +41,13 @@ class AaeEvaluationQuestion < ActiveRecord::Base
     when SCALE
       response.to_i
     when OPEN_DOLLAR_VALUE
-      response.gsub(%r{[^\d\.]},'').to_i
+      # use the mean of all the numbers we find
+      all_numerics = response.scan(%r{[\d\.,]+}).map{|i| i.tr(',','').to_i}
+      if(all_numerics.size >= 1)
+        all_numerics.mean.to_i
+      else
+        nil
+      end
     when OPEN_TIME_VALUE
       # interpret as days
       response.gsub(%r{[^\d]},'').to_i
